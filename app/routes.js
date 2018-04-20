1601b67be9925dbffccb266f142e60dd8c6aaf4d
Sandy Yin <sandyyin@vagelos-ve508-0782.apn.wlan.upenn.edu>
2018-04-20 13:43:17 -0400

    integrated queries except login queries

--- app/routes.js
+++ app/routes.js
@@ -1,11 +1,6 @@
 var mysql = require('mysql');
 module.exports = function(app) {
-  var connection = mysql.createConnection({
-    host: 'cis550project.c1plmbfccyny.us-east-1.rds.amazonaws.com',
-    port: '3306',
-    user: 'cis550project',
-    password: 'cis550project!'
-  });
+  var connection = mysql.createConnection({host: 'cis550project.c1plmbfccyny.us-east-1.rds.amazonaws.com', port: '3306', user: 'cis550project', password: 'cis550project!'});
 
   connection.connect(function(err) {
     if (err) {
@@ -19,7 +14,7 @@ module.exports = function(app) {
   // server routes ===========================================================
   // handle API calls
   app.get('/api/bookGenres', function(request, response) {
-    connection.query('SELECT genre, count(genre) FROM cis550.BOOK_GENRE group by genre having count(genre)>25 order by count(genre) desc;', function(err, res) {
+    connection.query('SELECT genre, count(genre) FROM cis550.BOOK_GENRES group by genre having count(genre)>25 order by count(genre) desc;', function(err, res) {
       if (err)
         response.send({err: err});
       var result = [];
@@ -45,12 +40,12 @@ module.exports = function(app) {
   });
 
   app.get('/api/popularBooks', function(request, response) {
-    connection.query('SELECT id, title, rating FROM cis550.book where rating >= 4 ORDER BY RAND() LIMIT 10;', function(err, res) {
+    connection.query('SELECT title, rating FROM cis550.BOOKS where rating >= 4 ORDER BY RAND() LIMIT 10;', function(err, res) {
       if (err)
         response.send({err: err});
       var result = [];
       res.forEach(function(item, index) {
-        result.push({name: item.title, rating: item.rating, index: item.id});
+        result.push({name: item.title, rating: item.rating});
       });
 
       response.send(result);
@@ -58,12 +53,12 @@ module.exports = function(app) {
   });
 
   app.get('/api/popularMovies', function(request, response) {
-    connection.query('SELECT id, title, rating FROM cis550.MOVIES where rating >= 8 ORDER BY RAND() LIMIT 10;', function(err, res) {
+    connection.query('SELECT title, rating FROM cis550.MOVIES where rating >= 8 ORDER BY RAND() LIMIT 10;', function(err, res) {
       if (err)
         response.send({err: err});
       var result = [];
       res.forEach(function(item, index) {
-        result.push({name: item.title, rating: item.rating, index: item.id});
+        result.push({name: item.title, rating: item.rating});
       });
 
       response.send(result);
@@ -71,97 +66,47 @@ module.exports = function(app) {
   });
 
   // added by Kara
-
-  /////////////////////////////////////////////////////////
-  //Guest
-  ///////////////////////////////////////////////////////////
-
-  // Search based on list of genres guest user inputs:book_genre -> books
-  app.get('/api/guestBookGenreSearch/:user', function(request, response) {
-    var result = [];
-    connection.query("SELECT b.title, b.rating FROM cis550.book b WHERE b.rating >= 4 AND b.id IN (SELECT DISTINCT(book_id) FROM cis550.BOOK_GENRE WHERE genre IN user=?) ORDER BY RAND() LIMIT 20", request.params.user, function(err, res) {
-      if (err)
-        response.send({err: err});
-      var bookResult = [];
-      res.forEach(function(item, index) {
-        bookResult.push({name: item.b.title, rating: item.b.rating});
-      });
-
-      result.push({books: bookResult});
-    });
-
-    connection.query("SELECT m.title, m.rating FROM cis550.MOVIES m WHERE m.rating >= 8 AND m.id IN (SELECT mg.movie_id FROM cis550.MOVIE_GENRES mg WHERE mg.genre IN (SELECT movie_genre FROM cis550.GENRES WHERE book_genre IN user=?))) ORDER BY RAND() LIMIT 20", request.params.user, function(err, res) {
-      if (err)
-        response.send({err: err});
-      var movieResult = [];
-      res.forEach(function(item, index) {
-        movieResult.push({name: item.m.title, rating: item.m.rating});
-      });
-
-      result.push({movies: movieResult});
-    });
-
-
-    response.send(result);
-  });
-
-  // Search based on list of genres guest user inputs:movie_genre -> books
-  app.get('/api/guestMovieGenreSearchBook/:user', function(request, response) {
-    connection.query("SELECT b.title, b.rating FROM cis550.book b WHERE b.rating >= 4 AND b.id IN (SELECT DISTINCT(bg.book_id) FROM cis550.BOOK_GENRE bg WHERE bg.genre IN (SELECT book_genre FROM cis550.GENRES WHERE movie_genre IN user=?)) ORDER BY RAND() LIMIT 20", request.params.user, function(err, res) {
+  app.get('/api/topRated', function(request, response) {
+    connection.query('SELECT TOP(10) b.title, m.title FROM cis550.BOOKS b, cis550.MOVIES m ORDER BY rating DESC;', function(err, res) {
       if (err)
         response.send({err: err});
       var result = [];
       res.forEach(function(item, index) {
-        result.push({name: item.b.title, rating: item.b.rating});
+        result.push({book_name: item.b.title, movie_name: item.m.title});
       });
 
       response.send(result);
     });
   });
 
-  // Search based on list of genres guest user inputs: movie_genre -> movies
-  app.get('/api/guestBookGenreSearchMovie/:user', function(request, response) {
-    connection.query("SELECT m.title, m.rating FROM cis550.MOVIES m WHERE m.rating >= 8 AND m.id IN (SELECT DISTINCT(movie_id) FROM cis550.MOVIE_GENRES WHERE genre IN user=?) ORDER BY RAND() LIMIT 20", request.params.user, function(err, res) {
+  // get user's favorites(their top 10% of all read or watched) Needs user_id
+  app.get('/api/userFavs/:user', function(request, response) {
+    connection.query('SELECT TOP 10 PERCENT br.title, mw.title FROM (SELECT r.user_id, b.title FROM cis550.BOOKS b JOIN cis550.READ r ON b.id = r.book_id WHERE user=? ORDER BY r.rating DESC) br, (SELECT w.user_id, m.title FROM cis550.MOVIES m JOIN cis550.WATCHED w ON m.id = w.movie_id WHERE user=? ORDER BY w.rating DESC) mw ', request.params.user, function(err, res) {
       if (err)
         response.send({err: err});
       var result = [];
       res.forEach(function(item, index) {
-        result.push({name: item.m.title, rating: item.m.rating});
+        result.push({Books: item.b.title, Movies: item.m.title});
       });
-
+      console.log(request.params.name);
       response.send(result);
     });
   });
 
-    // Random choice of book
-    app.get('/api/randBook', function(request, response) {
-      connection.query("SELECT title, rating FROM cis550.book ORDER BY RAND() LIMIT 1", function(err, res) {
-        if (err)
-          response.send({err: err});
-        var result = [];
-        res.forEach(function(item, index) {
-          result.push({name: item.title, rating: item.rating});
-        });
-
-        response.send(result);
+  // This users ratings compared to our average user's ratings Needs user_id
+  // OPTIONAL AND NOT COMPLETED YET
+  app.get('/api/userDiff/:user', function(request, response) {
+    connection.query('Select From READ r, Watched w', request.params.user, function(err, res) {
+      if (err)
+        response.send({err: err});
+      var result = [];
+      res.forEach(function(item, index) {
+        result.push({Books: item.b.title, Movies: item.m.title});
       });
+      console.log(request.params.name);
+      response.send(result);
     });
-
-    // Random choice of movie
-    app.get('/api/randBook', function(request, response) {
-      connection.query("SELECT title, rating FROM cis550.MOVIES ORDER BY RAND() LIMIT 1", function(err, res) {
-        if (err)
-          response.send({err: err});
-        var result = [];
-        res.forEach(function(item, index) {
-          result.push({name: item.title, rating: item.rating});
-        });
-
-        response.send(result);
-      });
-    });
-
-
+  });
 
   // added by Claire
 
@@ -172,7 +117,7 @@ module.exports = function(app) {
  // app.get('/api/userLogin/:user', function(request, response) {
  //    inDatabase = []
  //    connection.query('select exists(select * from cis550.USERS where cis550.USERS.email = "syin@gmail.com")', request.params.user, function(err,res))
-    
+ //    // 
  //    //create new user
  //    connection.query('INSERT INTO cis550.USERS VALUES (first_name, last_name, UUID(), email, password, gender)', request.params.user, function(err, res) {
     
@@ -192,13 +137,13 @@ module.exports = function(app) {
  //  });
 
   //DISPLAY WATCHLIST MOVIES
-  app.get('/api/watchlist', function(req, res) {
-    onnection.query('SELECT M.title as title, W.rating as user_rating, M.rating as avg_rating FROM cis550.WATCHED as W JOIN cis550.MOVIES as M ON W.movie_id = M.id WHERE user_id = "655"', request.params.user, function(err, res) {
+  app.get('/api/watchlist', function(request, response) {
+    connection.query('SELECT M.title as title, W.rating as user_rating, M.rating as avg_rating FROM cis550.WATCHED as W JOIN cis550.MOVIES as M ON W.movie_id = M.id WHERE user_id = "655"', request.params.user, function(err, res) {
       if (err)
         response.send({err: err});
       var result = [];
       res.forEach(function(item, index) {
-        result.push({Books: item.b.title, Movies: item.m.title});
+        result.push({Title: item.title, User_Rating: item.user_rating, Avg_Rating: item.avg_rating});
       });
       console.log(request.params.name);
       response.send(result);
@@ -206,58 +151,53 @@ module.exports = function(app) {
   });
 
   //DISPLAY READLIST BOOKS
-  app.get('/api/readlist', function(req, res) {
-    onnection.query('SELECT B.title, B.rating, R.rating FROM cis550.READ as R JOIN cis550.book as B ON R.book_id = B.id WHERE user_id = "655"', request.params.user, function(err, res) {
+  app.get('/api/readlist', function(request, response) {
+    connection.query('SELECT B.title, B.rating as avg_rating, R.rating as user_rating FROM cis550.READ as R JOIN cis550.book as B ON R.book_id = B.id WHERE user_id = "655"', request.params.user, function(err, res) {
       if (err)
         response.send({err: err});
       var result = [];
       res.forEach(function(item, index) {
-        result.push({Books: item.b.title, Movies: item.m.title});
+        result.push({Title: item.title, User_Rating: item.user_rating, Avg_Rating: item.avg_rating});
       });
       console.log(request.params.name);
       response.send(result);
     });
   });
 
-   //Add book to Readlist or update rating on book
-  app.get('/api/addToReadlist', function(req, res) {
-    // user id, rating, book_id
-    onnection.query('INSERT INTO cis550.READ VALUES (655, 5, 33) ON DUPLICATE KEY UPDATE rating = VALUES(rating);', request.params.user, function(err, res) {
+   //Add book to Readlist or update rating on book: user id, rating, book_id
+  app.get('/api/addToReadlist', function(request, response) {
+    //try to add down under 
+    connection.query('INSERT INTO cis550.READ VALUES (655, 1, 24) ON DUPLICATE KEY UPDATE rating = VALUES(rating);', function(err, res) {
+      console.log("err: ", err);
+      console.log("res: ", res);
       if (err)
         response.send({err: err});
-      var result = [];
-      res.forEach(function(item, index) {
-        result.push({Books: item.b.title, Movies: item.m.title});
-      });
-      console.log(request.params.name);
-      response.send(result);
+      response.send(res);
     });
   });
 
   //Add movie to watchlist or update rating on movie
-  app.get('/api/addToWatchlist', function(req, res) {
+  app.get('/api/addToWatchlist', function(request, response) {
     // user id, rating, book_id
-    onnection.query('INSERT INTO cis550.WATCHED VALUES (655, 8, 13) ON DUPLICATE KEY UPDATE rating = VALUES(rating)', request.params.user, function(err, res) {
+    //10: Simpson's Movie :) 
+   connection.query('INSERT INTO cis550.WATCHED VALUES (655, 10, 35) ON DUPLICATE KEY UPDATE rating = VALUES(rating)', request.params.user, function(err, res) {
       if (err)
         response.send({err: err});
       var result = [];
-      res.forEach(function(item, index) {
-        result.push({Books: item.b.title, Movies: item.m.title});
-      });
-      console.log(request.params.name);
       response.send(result);
     });
   });
 
   //Search for movies based on Name, return movie title and average rating
-  app.get('/api/searchMovie', function(req, res) {
+  app.get('/api/searchMovie', function(request, response) {
     // user id, rating, book_id
-    onnection.query('SELECT M.title, M.rating FROM cis550.MOVIES as M WHERE M.title LIKE "%Harry Potter% LIMIT 10', request.params.user, function(err, res) {
+    connection.query('SELECT M.title, M.rating FROM cis550.MOVIES as M WHERE M.title LIKE "%Harry Potter%" LIMIT 10', request.params.user, function(err, res) {
       if (err)
         response.send({err: err});
       var result = [];
-      res.forEach(function(item, index) {
-        result.push({Books: item.b.title, Movies: item.m.title});
+      res.forEach(function(item, index) { 
+        // console.log({Title: item.title, Rating: item.rating}) how to do this?
+        result.push({Title: item.title, Rating: item.rating});
       });
       console.log(request.params.name);
       response.send(result);
@@ -265,14 +205,14 @@ module.exports = function(app) {
   });
 
    //Search for books based on Name, return book title and average rating
-  app.get('/api/searchBook', function(req, res) {
+  app.get('/api/searchBook', function(request, response) {
     // user id, rating, book_id
-    onnection.query('SELECT B.title, B.rating FROM cis550.book as B WHERE B.title LIKE "%Harry Potter%" LIMIT 10', request.params.user, function(err, res) {
+    connection.query('SELECT B.title, B.rating FROM cis550.book as B WHERE B.title LIKE "%The Lord%" LIMIT 10', request.params.user, function(err, res) {
       if (err)
         response.send({err: err});
       var result = [];
       res.forEach(function(item, index) {
-        result.push({Books: item.b.title, Movies: item.m.title});
+        result.push({Title: item.title, Rating: item.rating});
       });
       console.log(request.params.name);
       response.send(result);
@@ -280,16 +220,16 @@ module.exports = function(app) {
   });
 
 
-  app.get('/api/allGenres', function(req, res) {
+  app.get('/api/allGenres', function(request, response) {
     res.send({'genre': 'something'});
   });
 
   // authentication routes
-  app.get('/api/login', function(req, res) {
+  app.get('/api/login', function(request, response) {
     res.send({'genre': 'something'});
   });
 
-  app.get('/api/register', function(req, res) {
+  app.get('/api/register', function(request, response) {
     res.send({'genre': 'something'});
   });
 
