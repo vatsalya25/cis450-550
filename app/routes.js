d71fd3892a9dafebb805d02d37580b7a0bf31d8a
Breeden <kbreeden@upenn.edu>
2018-04-20 16:15:17 -0400

    added tv genre searches and fixed some small errors

--- app/routes.js
+++ app/routes.js
@@ -163,15 +163,11 @@ module.exports = function(app) {
     });
   });
 
-
-  /////////////////////////////////////////////////////////
-  //User logged in
-  ///////////////////////////////////////////////////////////
-
-  // Search based on list of genres logged in user inputs:book_genre -> books
-  app.post('/api/loggedBookGenreSearch', function(request, response) {
+  // Search based on list of genres guest user inputs:tv_genre -> books
+  app.post('/api/guestTVGenreSearch', function(request, response) {
     var result = {};
-    var query1 = "SELECT b.id, b.title, b.rating FROM cis550.book b WHERE b.rating >= 4 AND b.id IN (SELECT DISTINCT(book_id) FROM cis550.BOOK_GENRE WHERE genre IN " + request.body.genres + ") AND b.id NOT IN (SELECT book_id FROM cis550.READ WHERE user_id =" + request.body.userId + ") ORDER BY RAND() LIMIT 20;";
+    var query1 = "SELECT b.title, b.rating FROM cis550.book b JOIN	(SELECT bg.book_id FROM cis550.BOOK_GENRE bg JOIN (SELECT g.book_genre FROM cis550.GENRES g JOIN (SELECT movie_genre FROM cis550.MOVIE_TV_GENRE WHERE tv_genre IN " + request.body.genres + ") mtg ON g.movie_genre = mtg.movie_genre) a ON bg.genre = a.book_genre) c ON b.id = c.book_id WHERE b.rating >= 4 ORDER BY RAND() LIMIT 20"
+    
 
     connection.query(query1, function(err, res) {
       if (err)
@@ -183,7 +179,45 @@ module.exports = function(app) {
 
       result.books = bookResult;
 
-      var query2 = "SELECT m.id, m.title, m.rating FROM cis550.movie m WHERE m.rating >= 4 AND m.id IN (SELECT mg.movie_id FROM cis550.MOVIE_GENRES mg WHERE mg.genre IN (SELECT movie_genre FROM cis550.GENRES WHERE book_genre IN " + request.body.genres + ")) AND m.id NOT IN (SELECT movie_id FROM cis550.WATCHED WHERE user_id = " + request.body.userId + ") ORDER BY RAND() LIMIT 20;"
+      // guest logged in tv-genre -> movies
+      var query2 = "SELECT m.title, m.rating FROM cis550.movie m JOIN	(SELECT mg.movie_id FROM cis550.MOVIE_GENRES mg JOIN	(SELECT movie_genre FROM cis550.MOVIE_TV_GENRE WHERE tv_genre IN '"+ request.body.genres + "') c ON mg.genre = c.movie_genre) d ON m.id = d.movie_id WHERE m.rating >= 4 ORDER BY RAND() LIMIT 20"
+
+      connection.query(query2, function(err, res) {
+        if (err)
+          response.send({err: err});
+        var movieResult = [];
+        res.forEach(function(item, index) {
+          movieResult.push({name: item.title, rating: item.rating, index: item.id});
+        });
+
+        result.movies = movieResult;
+
+        response.send(result);
+      });
+    });
+  });
+
+
+  /////////////////////////////////////////////////////////
+  //User logged in
+  ///////////////////////////////////////////////////////////
+
+  // Search based on list of genres logged in user inputs:book_genre -> books
+  app.post('/api/loggedBookGenreSearch', function(request, response) {
+    var result = {};
+    var query1 = "SELECT b.id, b.title, b.rating FROM cis550.book b WHERE b.rating >= 4 AND b.id IN (SELECT DISTINCT(book_id) FROM cis550.BOOK_GENRE WHERE genre IN " + request.body.genres + ") AND b.id NOT IN (SELECT book_id FROM cis550.READ WHERE user_id = '" + request.body.userId + "') ORDER BY RAND() LIMIT 20;";
+
+    connection.query(query1, function(err, res) {
+      if (err)
+        response.send({err: err});
+      var bookResult = [];
+      res.forEach(function(item, index) {
+        bookResult.push({name: item.title, rating: item.rating, index: item.id});
+      });
+
+      result.books = bookResult;
+
+      var query2 = "SELECT m.id, m.title, m.rating FROM cis550.movie m WHERE m.rating >= 4 AND m.id IN (SELECT mg.movie_id FROM cis550.MOVIE_GENRES mg WHERE mg.genre IN (SELECT movie_genre FROM cis550.GENRES WHERE book_genre IN " + request.body.genres + ")) AND m.id NOT IN (SELECT movie_id FROM cis550.WATCHED WHERE user_id = '" + request.body.userId + "') ORDER BY RAND() LIMIT 20;"
 
       connection.query(query2, function(err, res) {
         if (err)
@@ -203,7 +237,7 @@ module.exports = function(app) {
   // Search based on list of genres logged in user inputs:movie_genre -> books
   app.post('/api/loggedMovieGenreSearch', function(request, response) {
     var result = {};
-    var query1 = "SELECT b.title, b.rating FROM cis550.book b WHERE b.rating >= 4 AND b.id IN (SELECT DISTINCT(bg.book_id) FROM cis550.BOOK_GENRE bg WHERE bg.genre IN (SELECT book_genre FROM cis550.GENRES WHERE movie_genre IN " + request.body.genres + ")) AND b.id NOT IN (SELECT book_id FROM cis550.READ WHERE user_id = " + request.body.userId + ") ORDER BY RAND() LIMIT 20;";
+    var query1 = "SELECT b.title, b.rating FROM cis550.book b WHERE b.rating >= 4 AND b.id IN (SELECT DISTINCT(bg.book_id) FROM cis550.BOOK_GENRE bg WHERE bg.genre IN (SELECT book_genre FROM cis550.GENRES WHERE movie_genre IN " + request.body.genres + ")) AND b.id NOT IN (SELECT book_id FROM cis550.READ WHERE user_id = '" + request.body.userId + "') ORDER BY RAND() LIMIT 20;";
 
     connection.query(query1, function(err, res) {
       if (err)
@@ -214,7 +248,7 @@ module.exports = function(app) {
       });
 
       result.books = bookResult;
-      var query2 = "SELECT m.title, m.rating FROM cis550.movie m WHERE m.rating >= 4 AND m.id IN (SELECT DISTINCT(movie_id) FROM cis550.MOVIE_GENRES WHERE genre IN " + request.body.genres + ") AND m.id NOT IN (SELECT movie_id FROM cis550.WATCHED WHERE user_id =" + request.body.userId + ") ORDER BY RAND() LIMIT 20;"
+      var query2 = "SELECT m.title, m.rating FROM cis550.movie m WHERE m.rating >= 4 AND m.id IN (SELECT DISTINCT(movie_id) FROM cis550.MOVIE_GENRES WHERE genre IN " + request.body.genres + ") AND m.id NOT IN (SELECT movie_id FROM cis550.WATCHED WHERE user_id ='" + request.body.userId + "') ORDER BY RAND() LIMIT 20;"
 
       connection.query(query2, function(err, res) {
         if (err)
@@ -234,7 +268,7 @@ module.exports = function(app) {
   // Random choice of book for logged in user
   app.get('/api/randBookLogged', function(request, response) {
     var result = {};
-    var query1 = "SELECT title, rating FROM cis550.book WHERE id NOT IN (SELECT book_id FROM cis550.READ WHERE user_id = " + request.body.userId + ") ORDER BY RAND() LIMIT 10"
+    var query1 = "SELECT title, rating FROM cis550.book WHERE id NOT IN (SELECT book_id FROM cis550.READ WHERE user_id = '" + request.body.userId + "') ORDER BY RAND() LIMIT 10"
 
     connection.query(query1, function(err, res) {
       if (err)
@@ -266,7 +300,8 @@ module.exports = function(app) {
   // Search based on list of genres logged in user inputs:tv_genre -> books
   app.post('/api/LoggedTVGenreSearch', function(request, response) {
     var result = {};
-    var query1 = "SELECT b.id, b.title, b.rating FROM cis550.book b WHERE b.rating >= 4 AND b.id IN (SELECT DISTINCT(book_id) FROM cis550.BOOK_GENRE WHERE genre IN " + request.body.genres + ") AND b.id NOT IN (SELECT book_id FROM cis550.READ WHERE user_id =" + request.body.userId + ") ORDER BY RAND() LIMIT 20;";
+    var query1 = "SELECT b.title, b.rating FROM cis550.book b JOIN	(SELECT bg.book_id FROM cis550.BOOK_GENRE bg JOIN (SELECT g.book_genre FROM cis550.GENRES g JOIN (SELECT movie_genre FROM cis550.MOVIE_TV_GENRE WHERE tv_genre IN " + request.body.genres + ") mtg ON g.movie_genre = mtg.movie_genre) a ON bg.genre = a.book_genre) c ON b.id = c.book_id WHERE b.rating >= 4 AND b.id NOT IN (SELECT book_id FROM cis550.READ WHERE user_id = '" + request.body.userId + "') ORDER BY RAND() LIMIT 20"
+    
 
     connection.query(query1, function(err, res) {
       if (err)
@@ -278,7 +313,8 @@ module.exports = function(app) {
 
       result.books = bookResult;
 
-      var query2 = "SELECT m.id, m.title, m.rating FROM cis550.movie m WHERE m.rating >= 4 AND m.id IN (SELECT mg.movie_id FROM cis550.MOVIE_GENRES mg WHERE mg.genre IN (SELECT movie_genre FROM cis550.GENRES WHERE book_genre IN " + request.body.genres + ")) AND m.id NOT IN (SELECT movie_id FROM cis550.WATCHED WHERE user_id = " + request.body.userId + ") ORDER BY RAND() LIMIT 20;"
+      // tv-genre -> movies
+      var query2 = "SELECT m.title, m.rating FROM cis550.movie m JOIN	(SELECT mg.movie_id FROM cis550.MOVIE_GENRES mg JOIN	(SELECT movie_genre FROM cis550.MOVIE_TV_GENRE WHERE tv_genre IN '"+ request.body.genres + "') c ON mg.genre = c.movie_genre) d ON m.id = d.movie_id WHERE m.rating >= 4 AND m.id NOT IN (SELECT movie_id FROM cis550.WATCHED WHERE user_id = '" + request.body.userId + "') ORDER BY RAND() LIMIT 20"
 
       connection.query(query2, function(err, res) {
         if (err)
