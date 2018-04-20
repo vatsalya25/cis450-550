d2db1ab23e25a6b0f08376de6ef99473591b0c68
Breeden <kbreeden@upenn.edu>
2018-04-20 15:53:37 -0400

    Fixed names of api calls

--- app/routes.js
+++ app/routes.js
@@ -169,7 +169,7 @@ module.exports = function(app) {
   ///////////////////////////////////////////////////////////
 
   // Search based on list of genres logged in user inputs:book_genre -> books
-  app.post('/api/guestBookGenreSearch', function(request, response) {
+  app.post('/api/loggedBookGenreSearch', function(request, response) {
     var result = {};
     var query1 = "SELECT b.id, b.title, b.rating FROM cis550.book b WHERE b.rating >= 4 AND b.id IN (SELECT DISTINCT(book_id) FROM cis550.BOOK_GENRE WHERE genre IN " + request.body.genres + ") AND b.id NOT IN (SELECT book_id FROM cis550.READ WHERE user_id =" + request.body.userId + ") ORDER BY RAND() LIMIT 20;";
 
@@ -201,7 +201,7 @@ module.exports = function(app) {
   });
 
   // Search based on list of genres logged in user inputs:movie_genre -> books
-  app.post('/api/guestMovieGenreSearch', function(request, response) {
+  app.post('/api/loggedMovieGenreSearch', function(request, response) {
     var result = {};
     var query1 = "SELECT b.title, b.rating FROM cis550.book b WHERE b.rating >= 4 AND b.id IN (SELECT DISTINCT(bg.book_id) FROM cis550.BOOK_GENRE bg WHERE bg.genre IN (SELECT book_genre FROM cis550.GENRES WHERE movie_genre IN " + request.body.genres + ")) AND b.id NOT IN (SELECT book_id FROM cis550.READ WHERE user_id = " + request.body.userId + ") ORDER BY RAND() LIMIT 20;";
 
@@ -232,7 +232,7 @@ module.exports = function(app) {
   });
 
   // Random choice of book for logged in user
-  app.get('/api/randBook', function(request, response) {
+  app.get('/api/randBookLogged', function(request, response) {
     var result = {};
     var query1 = "SELECT title, rating FROM cis550.book WHERE id NOT IN (SELECT book_id FROM cis550.READ WHERE user_id = " + request.body.userId + ") ORDER BY RAND() LIMIT 10"
 
@@ -248,6 +248,69 @@ module.exports = function(app) {
 
       // Random choice of movie for logged in user
       connection.query("SELECT title, rating FROM cis550.movie WHERE id NOT IN (SELECT movie_id FROM cis550.WATCHED WHERE user_id = " + request.body.userId + ") ORDER BY RAND() LIMIT 10", function(err, res) {
+        if (err)
+        response.send({err: err});
+        var movieResult = [];
+        res.forEach(function(item, index) {
+          movieResult.push({name: item.title, rating: item.rating});
+        });
+
+        result.movies = movieResult;
+
+        response.send(result);
+      });
+    });
+  });
+
+  // Search based on list of genres logged in user inputs:tv_genre -> books
+  app.post('/api/LoggedTVGenreSearch', function(request, response) {
+    var result = {};
+    var query1 = "SELECT b.id, b.title, b.rating FROM cis550.book b WHERE b.rating >= 4 AND b.id IN (SELECT DISTINCT(book_id) FROM cis550.BOOK_GENRE WHERE genre IN " + request.body.genres + ") AND b.id NOT IN (SELECT book_id FROM cis550.READ WHERE user_id =" + request.body.userId + ") ORDER BY RAND() LIMIT 20;";
+
+    connection.query(query1, function(err, res) {
+      if (err)
+        response.send({err: err});
+      var bookResult = [];
+      res.forEach(function(item, index) {
+        bookResult.push({name: item.title, rating: item.rating, index: item.id});
+      });
+
+      result.books = bookResult;
+
+      var query2 = "SELECT m.id, m.title, m.rating FROM cis550.movie m WHERE m.rating >= 4 AND m.id IN (SELECT mg.movie_id FROM cis550.MOVIE_GENRES mg WHERE mg.genre IN (SELECT movie_genre FROM cis550.GENRES WHERE book_genre IN " + request.body.genres + ")) AND m.id NOT IN (SELECT movie_id FROM cis550.WATCHED WHERE user_id = " + request.body.userId + ") ORDER BY RAND() LIMIT 20;"
+
+      connection.query(query2, function(err, res) {
+        if (err)
+          response.send({err: err});
+        var movieResult = [];
+        res.forEach(function(item, index) {
+          movieResult.push({name: item.title, rating: item.rating, index: item.id});
+        });
+
+        result.movies = movieResult;
+
+        response.send(result);
+      });
+    });
+  });
+
+  // Search based on list of genres logged in user inputs:tv_genre -> books
+  app.post('/api/LoggedTVGenreSearch', function(request, response) {
+    var result = {};
+    var query1 = "SELECT b.title, b.rating FROM cis550.book b WHERE b.rating >= 4 AND b.id IN (SELECT DISTINCT(bg.book_id) FROM cis550.BOOK_GENRE bg WHERE bg.genre IN (SELECT book_genre FROM cis550.GENRES WHERE movie_genre IN " + request.body.genres + ")) AND b.id NOT IN (SELECT book_id FROM cis550.READ WHERE user_id = " + request.body.userId + ") ORDER BY RAND() LIMIT 20;";
+
+    connection.query(query1, function(err, res) {
+      if (err)
+        response.send({err: err});
+      var bookResult = [];
+      res.forEach(function(item, index) {
+        bookResult.push({name: item.title, rating: item.rating});
+      });
+
+      result.books = bookResult;
+      var query2 = "SELECT m.title, m.rating FROM cis550.movie m WHERE m.rating >= 4 AND m.id IN (SELECT DISTINCT(movie_id) FROM cis550.MOVIE_GENRES WHERE genre IN " + request.body.genres + ") AND m.id NOT IN (SELECT movie_id FROM cis550.WATCHED WHERE user_id =" + request.body.userId + ") ORDER BY RAND() LIMIT 20;"
+
+      connection.query(query2, function(err, res) {
         if (err)
         response.send({err: err});
         var movieResult = [];
