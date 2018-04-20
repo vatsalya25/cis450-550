614333f1bbb62c5be5ed194a9ee043ae6b7d900f
Claire Frankel <clairefrankel@vagelos-ve500-0812.apn.wlan.upenn.edu>
2018-04-20 13:45:30 -0400

    tested queries

--- app/routes.js
+++ app/routes.js
@@ -166,9 +166,41 @@ module.exports = function(app) {
   // added by Claire
 
   //**recommendations from BOOK search bar***
-  // book recs based on book
-      app.get('/api/bookSearch', function(request, response) {
-      connection.query("SELECT b.title as title1, b.authors, bg.genre, b.rating as rating1 FROM cis550.book b JOIN cis550.BOOK_GENRE bg ON b.id = bg.book_id JOIN (SELECT genre FROM cis550.BOOK_GENRE g WHERE book_id = (SELECT id FROM cis550.book WHERE title="The Dark Tower") ORDER BY count DESC LIMIT 2) g ON g.genre = bg.genre WHERE rating >=4 AND rating_count >=75000 ORDER BY RAND() LIMIT 10;", function(err, res) {
+      // book recs based on book
+    app.get('/api/bookSearch', function(request, response) {
+      var result = [];
+      connection.query("SELECT b.title as title1, b.authors, bg.genre, b.rating as rating1 FROM cis550.book b JOIN cis550.BOOK_GENRE bg ON b.id = bg.book_id JOIN (SELECT genre FROM cis550.BOOK_GENRE g WHERE book_id = (SELECT id FROM cis550.book WHERE title=\"The Dark Tower\") ORDER BY count DESC LIMIT 2) g ON g.genre = bg.genre WHERE rating >=4 AND rating_count >=75000 ORDER BY RAND() LIMIT 10;", function(err, res) {
+        if (err)
+          response.send({err: err});
+        var bookResult = [];
+        res.forEach(function(item, index) {
+          bookResult.push({name: item.title1, rating: item.rating1});
+        });
+
+        result.push({books: bookResult});
+    // movie recs based on book
+      connection.query("SELECT m.title as title1, mg.genre, m.rating as rating1 FROM cis550.MOVIES m JOIN cis550.MOVIE_GENRES mg ON m.id = mg.movie_id JOIN cis550.GENRES g ON g.movie_genre = mg.genre JOIN (SELECT genre FROM cis550.BOOK_GENRE WHERE book_id = (SELECT id FROM cis550.book WHERE title=\"The Dark Tower\") ORDER BY count DESC LIMIT 1) bg ON bg.genre = g.book_genre WHERE rating >=8 ORDER BY RAND() LIMIT 10; ", request.params.user, function(err, res) {
+        if (err)
+          response.send({err: err});
+        var movieResult = [];
+        res.forEach(function(item, index) {
+          movieResult.push({name: item.title1, rating: item.rating1});
+        });
+
+        result.push({movies: movieResult});
+
+      response.send(result);
+      
+      });
+    });
+  });
+
+
+  //***reommendations from MOVIE search bar****
+      // book recs based on movie
+    app.get('/api/movieSearch', function(request, response) {
+      var result = [];
+      connection.query("SELECT DISTINCT b.title as title1, b.authors, bg.genre, b.rating as rating1 FROM cis550.book b JOIN cis550.BOOK_GENRE bg ON b.id = bg.book_id JOIN cis550.GENRES g ON g.book_genre = bg.genre JOIN (SELECT genre FROM cis550.MOVIE_GENRES WHERE movie_id =(SELECT id FROM cis550.MOVIES WHERE title=\"Finding Nemo\") LIMIT 1) mg ON mg.genre = g.movie_genre WHERE rating >=4 ORDER BY RAND() LIMIT 10;", function(err, res) {
       if (err)
         response.send({err: err});
       var bookResult = [];
@@ -177,9 +209,9 @@ module.exports = function(app) {
       });
 
       result.push({books: bookResult});
-    });
-  // movie recs based on book
-      connection.query("SELECT m.title as title1, mg.genre, m.rating as rating1 FROM cis550.MOVIES m JOIN cis550.MOVIE_GENRES mg ON m.id = mg.movie_id JOIN cis550.GENRES g ON g.movie_genre = mg.genre JOIN (SELECT genre FROM cis550.BOOK_GENRE WHERE book_id = (SELECT id FROM cis550.book WHERE title="The Dark Tower") ORDER BY count DESC LIMIT 1) bg ON bg.genre = g.book_genre WHERE rating >=8 ORDER BY RAND() LIMIT 10; ", request.params.user, function(err, res) {
+
+      // movie recs based on movie
+      connection.query("SELECT m.title as title1, mg.genre, m.rating AS rating1 FROM cis550.MOVIES m JOIN cis550.MOVIE_GENRES mg ON m.id = mg.movie_id JOIN (SELECT genre FROM cis550.MOVIE_GENRES g WHERE movie_id = (SELECT id FROM cis550.MOVIES WHERE title=\"Finding Nemo\") LIMIT 1) g ON g.genre = mg.genre WHERE rating >=8 ORDER BY RAND() LIMIT 10;", request.params.user, function(err, res) {
       if (err)
         response.send({err: err});
       var movieResult = [];
@@ -188,40 +220,15 @@ module.exports = function(app) {
       });
 
       result.push({movies: movieResult});
+
+      response.send(result);
+    
+      }); 
     });
+  });
 
-    response.send(result);
-  };
 
-  //reommendations from MOVIE search bar
-  // book recs based on movie
-      app.get('/api/movieSearch', function(request, response) {
-      connection.query("SELECT DISTINCT b.title as title1, b.authors, bg.genre, b.rating as rating1 FROM cis550.book b JOIN cis550.BOOK_GENRE bg ON b.id = bg.book_id JOIN cis550.GENRES g ON g.book_genre = bg.genre JOIN (SELECT genre FROM cis550.MOVIE_GENRES WHERE movie_id =(SELECT id FROM cis550.MOVIES WHERE title="Finding Nemo") LIMIT 1) mg ON mg.genre = g.movie_genre WHERE rating >=4 ORDER BY RAND() LIMIT 10;", function(err, res) {
-      if (err)
-        response.send({err: err});
-      var bookResult = [];
-      res.forEach(function(item, index) {
-        bookResult.push({name: item.title1, rating: item.rating1});
-      });
-
-      result.push({books: bookResult});
-    });
-  // movie recs based on movie
-      connection.query("SELECT m.title as title1, mg.genre, m.rating AS rating1 FROM cis550.MOVIES m JOIN cis550.MOVIE_GENRES mg ON m.id = mg.movie_id JOIN (SELECT genre FROM cis550.MOVIE_GENRES g WHERE movie_id = (SELECT id FROM cis550.MOVIES WHERE title="Finding Nemo") LIMIT 1) g ON g.genre = mg.genre WHERE rating >=8 ORDER BY RAND() LIMIT 10;", request.params.user, function(err, res) {
-      if (err)
-        response.send({err: err});
-      var movieResult = [];
-      res.forEach(function(item, index) {
-        movieResult.push({name: item.title1, rating: item.rating1});
-      });
-
-      result.push({movies: movieResult});
-    });
-
-    response.send(result);
-  };
-
-  //user movies recs
+  //user movies recs (based on watched history)
       app.get('/api/userMovie', function(request, response) {
       connection.query("SELECT m.title as title1, mg.genre, m.rating as rating1 FROM cis550.MOVIES m JOIN cis550.MOVIE_GENRES mg ON m.id = mg.movie_id LEFT JOIN cis550.WATCHED w ON w.movie_id = m.id JOIN (SELECT AVG(rating) as r, genre FROM cis550.WATCHED w2 JOIN cis550.MOVIE_GENRES mg2 ON mg2.movie_id = w2.movie_id WHERE w2.user_id = 655 GROUP BY genre ORDER BY AVG(rating) DESC LIMIT 2) tg ON tg.genre = mg.genre WHERE m.rating >=8 ORDER BY RAND() LIMIT 10; ", function(err, res) {
         if (err)
@@ -235,7 +242,7 @@ module.exports = function(app) {
       });
     });
 
-  //user book recs
+  //user book recs (based on read history)
       app.get('/api/userBook', function(request, response) {
       connection.query("SELECT b.title as title1, bg.genre, b.rating as rating1 FROM cis550.book b JOIN cis550.BOOK_GENRE bg ON b.id = bg.book_id LEFT JOIN cis550.READ r ON r.book_id = b.id JOIN (SELECT AVG(rating) as r, genre FROM cis550.READ r2 JOIN cis550.BOOK_GENRE bg2 ON bg2.book_id = r2.book_id WHERE r2.user_id = 655 GROUP BY genre ORDER BY AVG(rating) DESC LIMIT 2) tg ON tg.genre = bg.genre WHERE b.rating >=4 AND b.rating_count >=75000 ORDER BY RAND() LIMIT 10; ", function(err, res) {
         if (err)
